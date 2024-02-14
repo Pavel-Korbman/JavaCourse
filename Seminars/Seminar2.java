ad7139bd3819cd06a89e8e4c490b71f34735eaa4
Pavel <pavel@b2profit.ru>
2024-02-14 21:45:01 +0300

    Файл 2 семинара

---
+++ Seminars/Seminar2.java
@@ -0,0 +1,18 @@
+import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Seminar2 {
+
+    public static void main(String[] args) {
+
+        System.out.println();
+
+    }
+
+    /* МОИ МЕТОДЫ*/
+
+
+
+}
