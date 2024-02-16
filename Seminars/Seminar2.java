bb41eb412e7a9583b1fd4dcb03776a4c65df477a
Pavel <pavel@b2profit.ru>
2024-02-16 19:07:55 +0300

    Запись 2 семинара

--- Seminars/Seminar2.java
+++ Seminars/Seminar2.java
@@ -1,19 +1,28 @@
 // Задачки на методы
 
 //import java.time.LocalTime;
-//import java.util.Arrays;
+
 //import java.util.Scanner;
-
-
-import java.util.Arrays;
+//import java.util.Arrays;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class Seminar2 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
 //        System.out.println(Task0(10, 'L', 'a'));
 //        System.out.println(Task1("caaaabbbaacdd"));
-        System.out.println(Task3("Ab1Ba"));
+//        System.out.println(Task3("Ab1Bb"));
+
+            ToFile(WordRepeat("TEST", 10));
+
+
+
 
     }
 
@@ -55,13 +64,68 @@ public class Seminar2 {
     Напишите метод, который принимает на вход строку (String)
     и определяет является ли строка палиндромом (возвращает boolean значение).
      */
-    static boolean Task3(String str) {
-        str = str.toLowerCase();
+//    static boolean Task3(String str) {
+//        str = str.toLowerCase();
+//        StringBuilder stringBuilder = new StringBuilder();
+//        stringBuilder.append(str);
+//        if (stringBuilder.reverse().toString().equals(str)) return true; // equals() сравнивает строки
+//        else return false;
+//    }
+
+    /* Задание №4
+  Напишите метод, который составит строку, состоящую из 100 повторений слова TEST
+  и метод, который запишет эту строку в простой текстовый файл, обработайте исключения.
+  Сделать логгирование.
+     */
+    static String WordRepeat(String word, int n) {
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(str);
-        if (stringBuilder.reverse().toString().equals(str)) return true; // equals() сравнивает строки
-        else return false;
+        for (int i = 0; i < n; i++) {
+            stringBuilder.append(word).append(' ');
+        }
+        stringBuilder.append("\n");
+        return stringBuilder.toString().trim();
+//        return word.repeat(n);  // Повторяет в строке слова n раз
     }
+//    Метод без логгера
+//    static void ToFile(String str) {
+//        try {
+//            FileWriter fileWriter = new FileWriter("sem2test.txt", true);
+//            fileWriter.write(str);  // Не запишет без .flush()
+//            fileWriter.append("\n");
+//            fileWriter.flush();
+//        } catch (IOException e) {
+//            e.printStackTrace();  // Выводит ошибку в терминал
+//            throw new RuntimeException(e);
+//        }
+
+        //    Метод с логгером
+    static void ToFile(String str) throws IOException {
+        Logger logger = Logger.getAnonymousLogger();  // Обычный логер, но без имени
+        FileHandler fileHandler = new FileHandler("log2.txt", true);
+        // Метод для записи логов в файл (true - логи будут дописываться),
+
+        logger.addHandler(fileHandler);  // Привязываем логер к файлу файлхендлера
+
+        SimpleFormatter simpleFormatter = new SimpleFormatter();
+        // Метод для форматирования логов - создаёт 2х строчные логи с осн информацией
+
+        fileHandler.setFormatter(simpleFormatter); // Привязываем форматер к хендлеру
+
+        try {
+                FileWriter fileWriter = new FileWriter("//sem2test.txt", true);
+                // Сделал ошибку в имени файла для логирования
+
+                fileWriter.write(str);  // Не запишет без .flush()
+                fileWriter.append("\n");
+                fileWriter.flush();
+            } catch (IOException e) {
+                e.printStackTrace();  // Выводит ошибку в терминал
+                logger.log(Level.INFO, e.getMessage());  // .getMessage() автоматически формирует сообщения об ошибке е.
+                throw new RuntimeException(e);
+            }
+
+    }
+
 
 
 }
