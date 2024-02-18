9c6041b645dccfa5e7d4cff4918151c217aac714
Pavel <pavel@b2profit.ru>
2024-02-18 11:57:49 +0300

    Файл 3 лекции

---
+++ Lec3.java
@@ -0,0 +1,27 @@
+//  ЛЕКЦИЯ 3.
+//  Коллекции
+
+import java.io.File;
+import java.io.IOException;
+
+public class Lec3 {
+    public static void main(String[] args) throws IOException {
+
+        //    Тип данных Object – универсальный тип данных.
+        //    Упаковка – любой тип можно положить в переменную типа Object
+        //    Распаковка – преобразование Object-переменной в нужный тип
+        //    Иерархия типов – любой тип «ниже» Object’а
+
+
+
+
+
+
+
+
+
+
+
+
+
+
