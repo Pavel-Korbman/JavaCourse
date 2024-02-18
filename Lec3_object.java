3b57cb47a03362ee515d55f03eef8a6885ea7eaa
Pavel <pavel@b2profit.ru>
2024-02-18 13:36:45 +0300

    Первый файл 3 лекции

--- Lec3_object.java
+++ Lec3_object.java
@@ -1,11 +1,6 @@
 //  ЛЕКЦИЯ 3.
 //  Коллекции
 
-//import java.io.File;
-//import java.io.IOException;
-
-//public class Lec3 {
-//    public static void main(String[] args) throws IOException {
 
 //    Тип данных Object – универсальный тип данных.
 //    Упаковка – любой тип можно положить в переменную типа Object
@@ -18,11 +13,65 @@ public class Lec3_object {
         GetType(o);    // java.lang.Integer
         o = 1.2;
         GetType(o);         // java.lang.Double
+
+        System.out.println(Sum(1, 2));
+        System.out.println(Sum(1.0, 2));
+        System.out.println(Sum(1, 2.0));
+        System.out.println(Sum(1.2, 2.1));
+        System.out.println(Sum("каша", "маша"));
+
+        //    Массивы
+//Проблема.Как увеличить размер массива?
+// Есть массив из 2 элементов
+        int[] a = new int[] { 1, 9 };
+        int[] b = new int[3];
+        System.arraycopy(a, 0, b, 0, a.length);  // Копирование элементов с нулевого из массива а в мас. b
+
+        for (int i : a) { System.out.printf("%d ", i);}  // 1 9
+        System.out.println();
+        for (int j : b) { System.out.printf("%d ", j);}  // 1 9 0
+        System.out.println();
+
+        // Добавляем элемент своим методом AddItem
+
+        int[] a1 = new int[] { 0, 9 };
+        for (int i : a1) { System.out.printf("%d ", i); } // 0 9
+        a1 = AddItem(a1, 2);
+        a1 = AddItem(a1, 3);
+        System.out.println();
+        for (int j : a1) { System.out.printf("%d ", j); }  //  0 9 2 3
+
+
+
     }
 
     static void GetType(Object obj) {
         System.out.println(obj.getClass().getName());
     }
+
+    static Object Sum(Object a, Object b) {
+        if (a instanceof Double && b instanceof Double) {  // Если a и b принадлежат типу Double
+            return  (Object)((Double) a + (Double) b);
+            // распаковываем a и b в тип Double, складываем и запаковываем обратно в Object
+
+        } else if(a instanceof Integer && b instanceof Integer) {  // Если a и b принадлежат типу Integer
+            return (Object)((Integer) a + (Integer) b);
+            // распаковываем a и b в тип Integer, складываем и запаковываем обратно в Object
+
+        } else return 0;
+    }
+
+    static int[] AddItem(int[] array, int item) {  // Добавление элемента к массиву
+        int length = array.length;
+        int[] temp = new int[length + 1];
+        System.arraycopy(array, 0, temp, 0, length);
+        temp[length] = item;
+        return temp;
+    }
+
+
+
+
 }
 
 
