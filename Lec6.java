d8d1e8d5293d7e6a4834a0785190335f68c57f81
Pavel <pavel@b2profit.ru>
2024-02-25 16:00:51 +0300

    Начало 6 лекции

--- Lec6.java
+++ Lec6.java
@@ -15,6 +15,11 @@ remove(V) – удаление элемента из коллекцию.
 contains(V) – проверка на включение элемента в коллекции.
 clear() – удаление всех элементов коллекции.
 size() – возвращает количество элементов коллекции.
+
+КАК СО МНОЖЕСТВАМИ:
+addAll(Coll) – объединение множеств.
+retainAll(Coll) – пересечение множеств.
+removeAll(Coll) – разность множеств.
  */
 
 
@@ -33,6 +38,17 @@ public class Lec6 {
         for (var item : set) System.out.println(item); // null 1 1234 123
         set.clear();
         System.out.println(set); // []
+
+        var a = new HashSet<>(Arrays.asList(1,2,3,4,5,6,7));
+        var b = new HashSet<>(Arrays.asList(2,3,5,7,11,13,17));
+        var u = new HashSet<Integer>(a); u.addAll(b);
+        var r = new HashSet<Integer>(a); r.retainAll(b);
+        var s = new HashSet<Integer>(a); s.removeAll(b);
+        System.out.println(a); // [1, 2, 3, 4, 5, 6]
+        System.out.println(b); // [17, 2, 3, 5, 7, 11]
+        System.out.println(u); // [1, 17, 2, 3, 4, 5, 6, 11]
+        System.out.println(r); // [2, 3, 5, 7]
+        System.out.println(s); // [1, 4, 6]
 
 
     }
