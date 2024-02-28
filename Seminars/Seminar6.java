e73a0d20b0ae508e69e01e89bd98f7bac3b26bf3
Pavel <pavel@b2profit.ru>
2024-02-28 22:54:36 +0300

    2я задача 6 семинара

--- Seminars/Seminar6.java
+++ Seminars/Seminar6.java
@@ -9,19 +9,24 @@ public class Seminar6 {
 
 
     public static void main(String[] args) {
-        task0();
+
+//        task0();
+
+        Integer[] array = createArr(1000, 0, 24);
+        //System.out.println(Arrays.toString(array));
+        System.out.println(task1(array));
     }
 
 
-     /* Задание №0
+    /* Задание №0
 1. Создайте HashSet, заполните его следующими числами: {1, 2, 3, 2, 4, 5, 6, 3}.
 Распечатайте содержимое данного множества.
 2. Создайте LinkedHashSet, заполните его следующими числами: {1, 2, 3, 2, 4, 5, 6, 3}.
 Распечатайте содержимое данного множества.
 3. Создайте TreeSet, заполните его следующими числами: {1, 2, 3, 2, 4, 5, 6, 3}.
 Распечатайте содержимое данного множества.
-         */
-    static void task0(){
+        */
+    static void task0() {
         Integer[] arr = {2, 9, 3, 8, 1, 2, 3, 8};
         String[] str = {"abvfffff", "bvm", "mvff", "avaaak"};
 
@@ -44,21 +49,37 @@ public class Seminar6 {
         System.out.println(set6);  // [abvfffff, avaaak, bvm, mvff] сортирует по алфавиту
 
 
-
-
-
-
     }
+
+    /* Задание №1
+  1. Напишите метод, который заполнит массив из 1000 элементов случайными цифрами от 0 до 24.
+  2. Создайте метод, в который передайте заполненный выше массив
+  и с помощью Set вычислите процент уникальных значений в данном массиве
+  и верните его в виде числа с плавающей запятой.
+  Для вычисления процента используйте формулу:
+  процент уникальных чисел = количество уникальных чисел * 100 / общее количество чисел в массиве.
+          */
+    static Integer[] createArr(int n, int min, int max) {
+        Integer[] arr = new Integer[n];
+        for (int i = 0; i <n; i++) {
+            arr[i] = new Random().nextInt(min, max + 1);
+        }
+        return arr;
+    }
+
+    static Double task1(Integer[] arr) {
+        var set = new HashSet<>(Arrays.asList(arr));
+        return (double) (set.size() * 100.0 / arr.length);
+    }
+
+
+
+    /* Задание №2*/
+
 }
 
 
 
-    /* Задание №1
-
-        */
-
-
-    /* Задание №2*/
 
 
 
